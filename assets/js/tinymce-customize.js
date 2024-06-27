6b61be62cc5180b524cce409c468fbad82b25dc4
unknown <sanjeevramanathan@gmail.com>
2024-06-27 05:52:54 +0530

    included lists - feat: #4 closed

--- assets/js/tinymce-customize.js
+++ assets/js/tinymce-customize.js
@@ -3,5 +3,7 @@ tinymce.init({
     selector: 'textarea#tiny', // textarea of the id 'tiny'
     menubar: false, // hiding the menubar 'File Edit ...'
     height: 300, // textarea height
-    toolbar: 'bold italic underline alignleft aligncenter alignright alignjustify'
+    plugins: 'lists', // 27 Jun 2024, 4:50 AM
+    // 'numlist bullist' added to the toolbar on 27 Jun 2024, 4:50 AM 
+    toolbar: 'bold italic underline alignleft aligncenter alignright alignjustify numlist bullist'
 });
